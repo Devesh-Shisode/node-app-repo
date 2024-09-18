c2748c4296adcb28c53ceea264d886b439a47fa1
Devesh-Shisode <deveshshisode@gmail.com>
2024-09-18 18:09:37 +0530

    os module code is ready

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # node-app-repo
 This is node app repository
+
+# Project build steps
+1. Clone the repository - <repo-url>
+2. npm install or npm update
+
+# Author
+Devesh Shisode
+7620 7333 20
---
+++ index.js
@@ -0,0 +1,14 @@
+const os=require('os');
+console.log("=============== built in OS-Module =================");
+ console.log(`arch : ${os.arch()}`);
+ console.log(`cpus : ${os.cpus()}`);
+ console.log(`freemem : ${os.freemem()}`);
+ console.log(`totalmem : ${os.totalmem()}`);
+ console.log(`hostname : ${os.hostname()}`);
+ console.log(`platform : ${os.platform()}`);
+ console.log(`release : ${os.release()}`);
+ console.log(`type : ${os.type()}`);
+ console.log(`uptime :${os.uptime()}`);
+console.log(`user Info : ${os.userInfo()}`);
+ 
+  
